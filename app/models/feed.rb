03cb1a73822a49783b72ad32e7224740607a1622
suzuki86 <tsnr0001@gmail.com>
2016-10-09 02:52:42 +0900

    Implement method to fetch feeds

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,24 @@
+require 'rss'
+
 class Feed < ActiveRecord::Base
+  def self.fetch_feeds(url)
+    RSS::Parser.parse(url).items
+  end
+
+  def self.save_feeds(feeds)
+    feeds.each do |feed|
+      if !Feed.find_by(url: feed.link)
+        feed = Feed.new(
+          title: feed.title,
+          url: feed.link,
+        )
+        feed.save
+      end
+    end
+  end
+
+  def self.update_feeds(url)
+    feeds = fetch_feeds(url)
+    save_feeds(feeds)
+  end
 end
