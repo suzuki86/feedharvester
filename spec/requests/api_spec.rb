7c590a6c839be6819610a472c742807b1c80e0c2
suzuki86 <tsnr0001@gmail.com>
2016-11-17 00:18:24 +0900

    Add spec for POST /api/v1/endpoints

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -30,4 +30,23 @@ describe API do
       expect(json[0]["title"]).to eq "TEST TITLE"
     end
   end
+
+  describe "POST /api/v1/endpoints" do
+    before do
+      FactoryGirl.create(:user_admin)
+      FactoryGirl.create(:apikey_admin)
+      FactoryGirl.create(:dummy_endpoints)
+      FactoryGirl.create(:dummy_feeds)
+    end
+
+    it "creates new entrypoint" do
+      headers = {
+        "Authorization" => "Bearer 398e5ff238efe5705dbca92b71a25c00"
+      }
+      post "/api/v1/endpoints", params: {name: "abcd", endpoint: "http://example.com/hoge1234"}, headers: headers
+      get "/api/v1/endpoints", headers: headers
+      json = JSON.parse(response.body)
+      expect(json[1]["name"]).to eq "abcd"
+    end
+  end
 end
