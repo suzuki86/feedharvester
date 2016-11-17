7204075d0690a7953f08a04881fa23a54d27bd53
suzuki86 <tsnr0001@gmail.com>
2016-11-18 00:04:55 +0900

    Add spec for GET /api/v1/endpoints

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 require 'json'
 
 describe API do
-  describe "GET /api/v1/feeds" do
-    before do
-      FactoryGirl.create(:user_admin)
-      FactoryGirl.create(:apikey_admin)
-      FactoryGirl.create(:dummy_endpoints)
-      FactoryGirl.create(:dummy_feeds)
-    end
+  before do
+    FactoryGirl.create(:user_admin)
+    FactoryGirl.create(:apikey_admin)
+    FactoryGirl.create(:dummy_endpoints)
+    FactoryGirl.create(:dummy_feeds)
+  end
 
+  let(:valid_token) do
+    {"Authorization" => "Bearer 398e5ff238efe5705dbca92b71a25c00"}
+  end
+
+  describe "GET /api/v1/feeds" do
     it "returns 401 with invalid token" do
       headers = {
         "Authorization" => "Bearer 1234"
@@ -20,9 +24,7 @@ describe API do
     end
 
     it "returns feeds with valid token" do
-      headers = {
-        "Authorization" => "Bearer 398e5ff238efe5705dbca92b71a25c00"
-      }
+      headers = valid_token
       get "/api/v1/feeds", headers: headers
       expect(response.status).to eq(200)
 
@@ -31,18 +33,18 @@ describe API do
     end
   end
 
-  describe "POST /api/v1/endpoints" do
-    before do
-      FactoryGirl.create(:user_admin)
-      FactoryGirl.create(:apikey_admin)
-      FactoryGirl.create(:dummy_endpoints)
-      FactoryGirl.create(:dummy_feeds)
-    end
-
+  describe "GET /api/v1/endpoints" do
     it "creates new entrypoint" do
-      headers = {
-        "Authorization" => "Bearer 398e5ff238efe5705dbca92b71a25c00"
-      }
+      headers = valid_token
+      get "/api/v1/endpoints", headers: headers
+      json = JSON.parse(response.body)
+      expect(json[0]["name"]).to eq "TEST ENDPOINT"
+    end
+  end
+
+  describe "POST /api/v1/endpoints" do
+    it "creates new entrypoint" do
+      headers = valid_token
       post "/api/v1/endpoints", params: {name: "abcd", endpoint: "http://example.com/hoge1234"}, headers: headers
       get "/api/v1/endpoints", headers: headers
       json = JSON.parse(response.body)
